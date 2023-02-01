9a30d9b00082fffb3aa0b1ad0b7b5ca19a69406d
bvt <bvt@uw.edu>
2023-01-31 22:12:49 -0800

    added some database constants

---
+++ src/constant/CommonConstants.ts
@@ -0,0 +1,18 @@
+/**
+ * Common constants.
+ */
+
+/**
+ * Database model name for Restaurant.
+ */
+export const RESTAURANT = "Restaurant";
+
+/**
+ * Database access account username.
+ */
+export const DATABASE_USERNAME = "dev";
+
+/**
+ * Database access account password.
+ */
+export const DATABASE_PASSWORD = "munch197";
