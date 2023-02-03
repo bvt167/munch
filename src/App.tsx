a1ac779cbe4c21056136fa301cc81c9083875b7c
Jasonj1-1920949 <gokujung3@gmail.com>
2023-02-03 15:35:07 -0800

    Changing frontend

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <header>
+          Munch: A Restaurant Boosting app
+        </header>
+        <p className="body_p">
+          Created by Jason and Brandon
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
