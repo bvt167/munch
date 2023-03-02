1f19eb5d80a0f2f14ab59043c49127f4254a04da
bvt <bvt@uw.edu>
2023-03-01 16:08:54 -0800

    removed database login

--- src/constant/CommonConstants.ts
+++ src/constant/CommonConstants.ts
@@ -6,13 +6,3 @@
  * Database model name for Restaurant.
  */
 export const RESTAURANT = "Restaurant";
-
-/**
- * Database access account username.
- */
-export const DATABASE_USERNAME = "dev";
-
-/**
- * Database access account password.
- */
-export const DATABASE_PASSWORD = "munch197";
