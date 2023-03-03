2f30a5f368f0e338623e1765da17e08548fac109
bvt <bvt@uw.edu>
2023-03-03 14:33:23 -0800

    refactored apiUtil

--- src/util/apiUtil.ts
+++ src/util/apiUtil.ts
@@ -14,24 +14,24 @@ const MODE_CORS = "cors";
 const SAME_ORIGIN_CREDENTIALS = "same-origin";
 
 export const loginAccount = async (params: any) => {
-  return await fetch(MUNCH_API_BASE_URL + LOGIN_ENDPOINT, {
-    method: POST, 
-    headers: {
-      "Content-Type": JSON_CONTENT_TYPE_HEADER,
-      "mode": MODE_CORS,
-      "credentials": SAME_ORIGIN_CREDENTIALS
-    },
-    body: JSON.stringify(params)
-  })
-  .then(checkStatus)
-  .then(resp => resp.json())
-  .catch(e => {
-    return { status: trimErrorPrefix(e.toString()) };
-  });
+  return await callMunchEndpoint(params, LOGIN_ENDPOINT);
 }
 
 export const registerAccount = async (params: any) => {
-  return await fetch(MUNCH_API_BASE_URL + REGISTER_ENDPOINT, {
+  return await callMunchEndpoint(params, REGISTER_ENDPOINT);
+}
+
+export const checkStatus = async (resp: any) => {
+  console.log(resp);
+  if (resp.ok) {
+    return resp;
+  }
+  resp = await resp.json();
+  throw Error(resp.status);
+};
+
+const callMunchEndpoint = async (params: any, endpoint: string) => {
+  return await fetch(MUNCH_API_BASE_URL + endpoint, {
     method: POST,
     headers: {
       "Content-Type": JSON_CONTENT_TYPE_HEADER,
@@ -46,15 +46,6 @@ export const registerAccount = async (params: any) => {
       return { status: trimErrorPrefix(e.toString()) };
     });
 }
-
-export const checkStatus = async (resp: any) => {
-  console.log(resp);
-  if (resp.ok) {
-    return resp;
-  }
-  resp = await resp.json();
-  throw Error(resp.status);
-};
 
 const trimErrorPrefix = (msg: string): string => {
   return msg.replace("Error: ", "");
