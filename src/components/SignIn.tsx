3b4fe4a5fc404bf7e7ad20e40ae6366da34f8794
bvt <bvt@uw.edu>
2023-04-06 19:37:39 -0700

    added css rule to make body and root div take up entire height of the screen

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 
 const SignIn = () => {
   return (
-    <div className="container">
-      <div className="row align-items-center">
-        <div className="col">
-          <h1 className='pl-4'>Welcome to</h1>
+    <div className="container d-flex align-items-center h-100">
+      <div className='flex-grow-1 align-items-center'>
+        <div>
+          <h1 className='ps-4'>Welcome to</h1>
           <img className='m-0 p-0' src='../../assets/munchLogo.png' alt='Munch Logo'></img>
         </div>
       </div>
     </div>
+    // <div className='d-flex align-items-center justify-content-center'>
+    //   <div>
+    //     <h1 className='ps-4'>Welcome to</h1>
+    //     <img className='m-0 p-0' src='../../assets/munchLogo.png' alt='Munch Logo'></img>
+    //   </div>
+    // </div>
   );
 }
 
