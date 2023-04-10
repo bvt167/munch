72c0061db2a7b16f52492696924caf8f61fcbedf
Jasonj1-1920949 <gokujung3@gmail.com>
2023-04-09 22:59:09 -0700

    Updating the Home Page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Cards = styled.div`
-    border-radius: 1px; 
+    border-collapse: seperate;
+    border-radius: 10px;
     margin-bottom: 50px;
     margin-top: 25px;
+    box-shadow: 0px 0px 5px 5px #D3D3D3
 `
 
 
@@ -22,8 +24,8 @@ const Home = (props) => {
             <Navbar />
             <Link to="/publish">
                 <Cards>
-                <div className="card">
-                    <img src="../../assets/image1.png" alt="Avatar"></img>
+                <div className="card" style={{borderRadius: "10px"}}>
+                    <img src="../../assets/image1.png" alt="Avatar" style={{borderRadius: "10px"}}></img>
                     <div className="container">
                         <h4 className="text-center"> Make new post </h4>
                     </div>
@@ -31,19 +33,23 @@ const Home = (props) => {
                 </Cards>
             </Link>
             <Link to="/viewpost">
+                <Cards>
                 <div className="card">
                     <img src="../../assets/image2.png" alt="Avatar"></img>
                     <div className="container">
                         <h4 className="text-center"> View all posts </h4>
                     </div>
                 </div>
+                </Cards>
             </Link>
+            <Cards>
             <div className="card">
                 <img src="../../assets/image3.png" alt="Avatar"></img>
                 <div className="container">
                     <h4 className="text-center"> Tips for posting </h4>
                 </div>
             </div>
+            </Cards>
         </div>
     )
 }
