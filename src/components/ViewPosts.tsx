713db0bdc3bba6928491e478df4e239f1462aee1
Jasonj1-1920949 <gokujung3@gmail.com>
2023-04-12 22:07:33 -0700

    Fixing view post page

--- src/components/ViewPosts.tsx
+++ src/components/ViewPosts.tsx
@@ -15,7 +15,7 @@ const Image = styled.div`
     width: 100px;
     display: flex;
     align-items: center;
-    margin-right: 35px;
+    margin-right: 3px;
 `
 
 const Button = styled.div`
@@ -39,39 +39,29 @@ const ProfileName = styled.div`
 const ViewPost = (props) => {
 
     return (
-        <div className="container">
+        <div className="container" style={{maxWidth: "93.5rem", margin: "0 auto", padding: "0 1rem"}}>
 
         <Body>
             <div className="profile" style={{padding: "1rem 0"}}>
                 <Image>
-                    <img src="https://images.unsplash.com/photo-1513721032312-6a18a42c8763?w=152&h=152&fit=crop&crop=faces" alt="" style={{borderRadius: "50%", width: "95px", height: "95px"}}></img>
+                    <img src="https://images.unsplash.com/photo-1513721032312-6a18a42c8763?w=152&h=152&fit=crop&crop=faces" alt="" style={{display: "block", borderRadius: "50%", width: "75px", height: "75px"}}></img>
                 </Image>
-                <div className="profile-user-settings" style={{float: "left", width: "calc(66.666% - 2rem)", marginTop: "25px"}}>
+                <div className="profile-user-settings" style={{float: "left", marginTop: "25px"}}>
 
-                    <h1 className="profile-user-name" style={{display: "inline-block", fontSize: "25px", fontWeight: "300"}}>MunchRestaurants</h1>
+                    <h1 className="profile-user-name" style={{display: "inline-block", fontSize: "15px", fontWeight: "300"}}>MunchRestaurants</h1>
 
-                    <button className="btn profile-edit-btn" style={{fontSize: "1rem", lineHeight: "1.8", border: "0.1rem solid #dbdbdb", borderRadius: "0.3rem", padding: "0 2.4rem", marginLeft: "2rem"}}>Edit Profile</button>
-
-                    <Button><button className="btn profile-settings-btn" aria-label="profile settings"><i className="fas fa-cog" aria-hidden="true"></i></button></Button>
-
+                    <button className="btn profile-edit-btn" style={{display: "inline-block", font: "inherit", background: "none", color: "inherit", cursor: "pointer", fontSize: "8px", lineHeight: "1.8", border: "0.1rem solid #dbdbdb", borderRadius: "0.3rem", padding: "0 1rem", marginLeft: "5px"}}>Edit Profile</button>
                 </div>
 
-                <div className="profile-stats" style={{float: "left", width: "calc(66.666% - 2rem)", marginTop: ""}}>
+                <div className="profile-stats" style={{float: "left", width: "", marginTop: "1rem"}}>
 
                     <ul>
-                        <li><span className="profile-stat-count">164</span> posts</li>
-                        <li><span className="profile-stat-count">188</span> followers</li>
-                        <li><span className="profile-stat-count">206</span> following</li>
+                        <li style={{display: "inline-block", fontSize: "12px", lineHeight: "1", marginRight: "1rem", cursor: "pointer"}}><span className="profile-stat-count">164</span> posts</li>
+                        <li style={{display: "inline-block", fontSize: "12px", lineHeight: "1", marginRight: "1rem", cursor: "pointer"}}><span className="profile-stat-count">188</span> followers</li>
+                        <li style={{display: "inline-block", fontSize: "12px", lineHeight: "1", marginRight: "1rem", cursor: "pointer"}}><span className="profile-stat-count">206</span> following</li>
                     </ul>
 
                 </div>
-
-                <div className="profile-bio" style={{float: "left", width: "calc(66.666% - 2rem)"}}>
-
-                    <p><span className="profile-real-name">Jane Doe</span> Lorem ipsum dolor sit, amet consectetur adipisicing elit</p>
-
-                </div>
-
             </div>
         </Body>
         </div>
