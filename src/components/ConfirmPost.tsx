526c10ce3c27cb50a7047493551e73a516c48213
bvt <bvt@uw.edu>
2023-04-13 19:09:20 -0700

    implemented show confirm page after delay in ConfirmPost

--- src/components/ConfirmPost.tsx
+++ src/components/ConfirmPost.tsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 
 
 const ConfirmPost = () => {
-    return (
-        <div className="container-fluid p-0"> 
-            <div id="loader"></div>
 
-            <div style={{}} id="myDiv" className="animate-bottom">
-            <h2>Tada!</h2>
-            <p>Some text in my newly loaded page..</p>
+  const [loaderDisplay, setLoaderDisplay] = useState("block");
+  const [pageDisplay, setPageDisplay] = useState("none");
+
+  const showPageAfterDelay = useCallback(() => {
+    setTimeout(showPage, 3000);
+  }, []);
+
+  const showPage = () => {
+    setLoaderDisplay("none");
+    setPageDisplay("block");
+  }
+
+  useEffect(() => {
+    showPageAfterDelay();
+  }, [showPageAfterDelay]);
+
+    return (
+        <div className="container-fluid p-0">
+            <div id="loader" style={ {display: loaderDisplay} }></div>
+
+            <div style={ {display: pageDisplay} } id="myDiv" className="animate-bottom">
+              <h2>Tada!</h2>
+              <p>Some text in my newly loaded page..</p>
             </div>
 
         </div>
