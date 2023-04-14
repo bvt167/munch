3dad5e6be56d3ba4e86a7af5ee43beae59935b4b
Jasonj1-1920949 <gokujung3@gmail.com>
2023-04-14 13:19:45 -0700

    Finished publish and confirm post

--- src/components/ConfirmPost.tsx
+++ src/components/ConfirmPost.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 
@@ -6,6 +7,8 @@ const ConfirmPost = () => {
 
   const [loaderDisplay, setLoaderDisplay] = useState("block");
   const [pageDisplay, setPageDisplay] = useState("none");
+  const navigate = useNavigate();
+
 
   const showPageAfterDelay = useCallback(() => {
     setTimeout(showPage, 3000);
@@ -21,14 +24,19 @@ const ConfirmPost = () => {
   }, [showPageAfterDelay]);
 
     return (
-        <div className="container-fluid p-0">
+      <div className="container-fluid p-0"> 
+      <div className="p-0" style={{ width: "390px", height: "95px", background: "linear-gradient(210deg, #FFDD95, #C72785, #6F39C5, #2F46F3 400%)", position: 'absolute', zIndex: -1, marginBottom: "100px", top: "0"}}>
+          <img src="../../assets/image4.png" alt="Avatar" style={{width: "390px", marginTop: "55px", borderRadius: "25px"}}></img>
+      </div>
             <div id="loader" style={ {display: loaderDisplay} }></div>
 
             <div style={ {display: pageDisplay} } id="myDiv" className="animate-bottom">
-              <h2>Tada!</h2>
-              <p>Some text in my newly loaded page..</p>
+              <h2 style ={{marginTop: "225px", fontWeight: "bold"}}>YAY!</h2>
+              <p style={{fontSize: "20px", marginBottom: "1px"}}>People can now see your</p>
+              <p style={{fontSize: "20px"}}>post on Instagram.</p>
+              <p style={{fontSize: "20px", marginTop:"100px", marginBottom: "5px"}}>To view a post or post again,</p>
+              <p style={{fontSize: "20px", fontWeight: "bold"}} onClick={() => navigate("/home")}>Tap here to go back home!</p>
             </div>
-
         </div>
     );
 };
