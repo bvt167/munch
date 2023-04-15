2f6abc93d9f5235802630e76b0d46d5a3cdad646
bvt <bvt@uw.edu>
2023-04-14 21:19:52 -0700

    added background to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,13 +12,16 @@ const Navbar = (props) => {
     const navigate = useNavigate();
 
     return (
-        <div className="container p-0" style={{background: "white"}}>
-            <IconButton className="homebutton" style={{background: "pink", position: "fixed", marginLeft: "115px", marginBottom: "25px", bottom: "0", width: "55px", height: "55px"}} aria-label="back" onClick={() => navigate("/home")}>
+        <div className="container p-0" style={{}}>
+          <div>
+            <IconButton className="homebutton" style={{background: "pink", position: "fixed", marginLeft: "115px", marginBottom: "15px", bottom: "0", width: "55px", height: "55px"}} aria-label="back" onClick={() => navigate("/home")}>
                 <HomeIcon htmlColor="#FFFFFF" style={{width: "35px", height: "35px"}} />
             </IconButton>
-            <IconButton className="homebutton" style={{background: "pink", position: "fixed", marginLeft: "215px", marginBottom: "25px", bottom: "0", width: "55px", height: "55px"}} aria-label="back" onClick={() => navigate("/home")}>
+            <IconButton className="homebutton" style={{background: "pink", position: "fixed", marginLeft: "215px", marginBottom: "15px", bottom: "0", width: "55px", height: "55px"}} aria-label="back" onClick={() => navigate("/home")}>
                 <AccountCircleIcon htmlColor="#FFFFFF" style={{width: "35px", height: "35px"}}/>
             </IconButton>
+          </div>
+          <div className="bg-white" style={{position: "fixed", width: "100%", height: "10%", bottom: "0", borderTop: "solid", borderRadius: "10px"}}></div>
         </div>
     );
 };
